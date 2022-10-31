5b5671c2b3bf7e63684c31f6bbc9e76448ea9e64
Alexander Sorokin <alex_grom@yahoo.com>
2022-10-31 13:01:54 +0000

    TTS working. WIP. File names are no unique, not text normalization. API is to be changed

--- README.md
+++ README.md
@@ -5,7 +5,8 @@
 
 - [x] docker packaging
 - [x] make initial server
-- [ ] silero package and models cached in docker
+- [x] silero package working
+- [ ] models cached in docker
 - [ ] server implements HA TTS HTTP API https://www.home-assistant.io/integrations/tts/
 - [ ] TTS code itself
 - [ ] caching of TTS results in local filesystem
@@ -16,3 +17,9 @@
 ## I took some ideas from
 
 * https://github.com/elia-morrison/silero_docker
+
+## example request for testing
+
+```
+curl -X POST -d '{"text":"жопа с ручкой"}'  -H  "accept: application/json" -H  "Content-Type: application/json" http://localhost:9898/api
+```
--- server.py
+++ server.py
@@ -1,8 +1,42 @@
-from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+
+import os
+import torch
+
+from typing import Any, Dict, AnyStr, List, Union
+
+
+JSONObject = Dict[AnyStr, Any]
+JSONArray = List[Any]
+JSONStructure = Union[JSONArray, JSONObject]
+
+
+device = torch.device('cpu')
+torch.set_num_threads(4)
+local_file = 'model.pt'
+speaker = 'xenia'  # 'aidar', 'baya', 'kseniya', 'xenia', 'random'
+sample_rate = 48000  # 8000, 24000, 48000
+
+
+# put this in docker image preparation
+if not os.path.isfile(local_file):
+    torch.hub.download_url_to_file('https://models.silero.ai/models/tts/ru/ru_v3.pt',
+                                   local_file)  
+
+
+model = torch.package.PackageImporter(local_file).load_pickle("tts_models", "model")
+model.to(device)
+
 
 app = FastAPI()
 
+try:
+    os.mkdir("./static")
+except:
+    pass
+
+app.mount("/static", StaticFiles(directory="./static"), name="static")
 
 @app.get("/")
 async def read_root():
@@ -11,3 +45,18 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+#curl -x POST -d '{"text":"posdf"}'  -H  "accept: application/json" -H  "Content-Type: application/json" 127.0.0.1:9898/api
+
+@app.post("/api")
+async def tts(request: Request):
+    json = await request.json()
+    #audio = model.apply_tts(text=example_text, speaker=speaker, sample_rate=sample_rate)
+
+
+    # (text=None, ssml_text=None, speaker: str = 'xenia', audio_path: str = '', sample_rate: int = 48000, put_accent=True, put_yo=True)
+    audio_path = "output.wav"
+    res_path = model.save_wav(text=json["text"],speaker=speaker, audio_path="./static/"+audio_path, sample_rate=sample_rate)
+
+    return {"url": request.url_for("static", path=audio_path)}
